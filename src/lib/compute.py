82c3aab666b0847f50a72af94e81eb0bcf93e28b
sanix sandel <sanicksikani@gmail.com>
2020-08-26 00:31:21 +0300

    Add a class for math functions

---
+++ src/lib/compute.py
@@ -0,0 +1,16 @@
+class Compute:
+    def __init__(self, operator, operands):
+        self.operator=operator
+        self.operands=operands
+
+    def add(self):
+        pass
+
+    def substract(self):
+        pass
+
+    def divise(self):
+        pass
+
+    def multiply(self):
+        pass    
